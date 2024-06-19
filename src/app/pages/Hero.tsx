e08c60a428b69d8006dcd76cb3a595de49483d68
burakbehlull <burakyabgu@gmail.com>
2024-06-19 13:45:32 +0300

    feat: Complete main styles

--- src/app/pages/Hero.tsx
+++ src/app/pages/Hero.tsx
@@ -4,20 +4,24 @@ const Hero : React.FC = ()=> {
         <section id='hero'>
             <nav>
                 <ul>
-                    <li>LOCALITON</li>
-                    <li>MEKTUP</li>
-                    <li>CALL</li>
+                    <li>LOCAL</li>
+                    <li>CV</li>
+                    <li>NAVIGATE</li>
                 </ul>
             </nav>
 
             <article>
-                <h1>ARTICLE</h1>
+                <div>
+                    <p>available for freelance</p>
+                    <h1>BURAK</h1>
+                    <h1>BEHLÜL</h1>
+                </div>
             </article>
 
             <footer>
                 <ul>
-                    <li>BASED IN CALIFORNA</li>
-                    <li>DIGITAL DESGINER</li>
+                    <li>BASED IN İSTANBUL</li>
+                    <li>FULLSTACK DEVELOPER</li>
                 </ul>
             </footer>
         </section>
